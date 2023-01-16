d9286f4a9590eed0b62563e937371f6e8d2c9a09
KichiroNakamoto <santiago.ruizm40@gmail.com>
2023-01-15 21:55:19 -0500

    uploading basic structure of html, css and js

---
+++ pong.js
@@ -0,0 +1,17 @@
+// Set up canvas and game variables
+var canvas = document.getElementById("game");
+var ctx = canvas.getContext("2d");
+var ballX = canvas.width / 2;
+var ballY = canvas.height / 2;
+var ballRadius = 10;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
+var player1Y = (canvas.height - 100) / 2;
+var player2Y = (canvas.height - 100) / 2;
+var playerWidth = 10;
+var playerHeight = 100;
+var playerSpeed = 5;
+var player1Score = 0;
+var player2Score = 0;
+
+
